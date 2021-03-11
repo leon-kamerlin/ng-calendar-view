cb5a6772e6f544c664606dc64ba54b71c13f7b7d
= <=>
2021-03-11 02:22:16 +0100

    0.0.3

--- projects/calendar-view-example/src/app/app.module.ts
+++ projects/calendar-view-example/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FullcalendarModule } from 'calendar-view';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FullcalendarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- projects/calendar-view/src/public-api.ts
+++ projects/calendar-view/src/public-api.ts
@@ -2,9 +2,6 @@
  * Public API Surface of calendar-view
  */
 
-export * from './lib/calendar-view.component';
-export * from './lib/calendar-view.module';
-
 
 export * from './lib/fullcalendar/fullcalendar.component';
 export * from './lib/fullcalendar/fullcalendar.module';
